f00f84db8cf25e380c6c9c44ece0260e5111e279
Francois Keith <keith@lirmm.fr>
2014-02-10 18:33:11 +0100

    Correct constraint parameter listener.

--- scripts/taskParameter2sot.py
+++ scripts/taskParameter2sot.py
@@ -4,7 +4,7 @@ roslib.load_manifest('constraint_msgs')
 import rospy
 import actionlib
 
-from constraint_msgs.msg import ConstraintConfig, Constraint, Feature
+from constraint_msgs.msg import ConstraintCommand
 
 roslib.load_manifest('dynamic_graph_actionlib')
 from dynamic_graph_actionlib.msg import *
@@ -14,15 +14,20 @@ roslib.load_manifest ('dynamic_graph_bridge')
 from dynamic_graph_bridge.srv import RunCommand
 
 """ convert a vector3 to a string """
-def vector3ToStr(vec):
-    st = "(%f, %f, %f)" % (vec.x, vec.y, vec.z)
-    return st;
+def vectorToStr(vec):
+    rospy.loginfo(rospy.get_name() + ": I heard %d" % len(vec))
+    st = '('
+    for i in range(0, len(vec)):
+      s = "%f, " % vec[i]
+      st = st + s
+    st = st + ')'
+    return st
 
 """ run an inscruction """
 def runCommand(proxy, instruction):
     rospy.loginfo ("run instruction: " + instruction)
     result = proxy (instruction)
-    rospy.loginfo ("stdout: " + result.stdout)
+#    rospy.loginfo ("stdout: " + result.stdout)
     rospy.loginfo ("stderr: " + result.stderr)
 
 
@@ -39,7 +44,16 @@ def createConstraint(proxy, command):
 
 
 def convertContraintToCommands(proxy, c):
-  rospy.loginfo(": Working the constraint %s" % (c.controller_id))
+  rospy.loginfo(": Working Beta the constraint %s" % (c.controller_id))
+  instruction = "setTaskGoal(robot, '"+c.controller_id+"', " +\
+    vectorToStr(c.pos_lo) + ", " + vectorToStr(c.pos_hi) + ", " +\
+    "'" + c.selec + "'" + ")"
+  runCommand(proxy, instruction)
+#  if c.selec != "":
+#    instruction3 = "robot.features['"+c.controller_id+"'].selec.value = '" + c.selec + "'" # Xmin, Ymin
+#    runCommand(proxy, instruction3)
+
+  # TODO: gain....
 #     if(lowerBound == upperBound):
 #      feature.reference.value = lowerBound
 #    else:
@@ -47,7 +61,6 @@ def convertContraintToCommands(proxy, c):
 #      task.referenceInf.value = lowerBound
 #      task.referenceSup.value = upperBound
 #  instruction = "robot.features['"+ command.name + "'].reference.value = 
-  runCommand(proxy, instruction)
 
 # Allows to modify the state of a constraint online
 # string identifier for the controller used
@@ -65,15 +78,16 @@ def convertContraintToCommands(proxy, c):
 ConstraintListener listens to the constraints message sent, and converts
 them into commands sent to the stack of tasks run_command interface.
 """
-class Listener:
+class ConstraintConfigListener:
 
     run_command = None
 
     def __init__(self):
         # Create the node
-        rospy.init_node('listener', anonymous=True)
+        rospy.init_node('constraint_config', anonymous=True)
 
         # Wait for the run_command service to be started
+        rospy.loginfo("\n " + rospy.get_name() + " waiting for run_command")
         rospy.wait_for_service ('run_command')
         rospy.loginfo(rospy.get_name() + "run_command obtained")
         self.run_command = rospy.ServiceProxy ('run_command', RunCommand)
@@ -85,16 +99,16 @@ class Listener:
         runCommand(self.run_command, "from dynamic_graph.sot.dyninv import  TaskInequality")
 
         # Subscribe to the constraint publisher
-        rospy.Subscriber("/constraint_config", ConstraintConfig, self.callback)
+        rospy.Subscriber("/constraint_command", ConstraintCommand, self.callback)
         rospy.spin()
 
     def callback(self, data):
         rospy.loginfo(rospy.get_name() + ": I heard %s" % data.controller_id)
 
         # convert the constrain and the send the corresponding command
-        convertContraintCommandToPython(self.run_command, data.constraints)
+        convertContraintToCommands(self.run_command, data)
 
 # Start the listener
 if __name__ == '__main__':
-    talker = Listener ()
+    talker = ConstraintConfigListener ()
 
